6b1f29d7e39024adcd629f7c0dee5494f41ecb56
tacogips <me@tacogips.me>
2019-12-16 15:42:38 +0900

    some

--- keymap.c
+++ keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |  Q  |   W  |   E  |   R   |  T   |  `   |                   |  "   |  Y  |   U  |   I  |   O  |   P  |    [   |
    * |------+-----+------+------+------+-------+------+--------------------+------+------+------+------+------+------+------|
-   * | {    |  A   |  S  |  D  |   F   |   G   |  ESC |                   | *    | H  |   J  |   K  |   L  |   ;  |    ]    |
+   * | GUI  |  A   |  S  |  D  |   F   |   G   |  ESC |                   | *    | H  |   J  |   K  |   L  |   ;  |    ]    |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | }    | Sfhit|  Z  |   X  |   C  |   V   |  B   |                   |  %   | N  |   M  |   ,  |   .  |   /     |  Up  |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,     KC_2,     KC_3,   KC_4,  KC_5,    KC_6,     								KC_EQL,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSLS,     \
     KC_TAB,  KC_Q,     KC_W,     KC_E,   KC_R,  KC_T,    KC_GRV,                  KC_QUOT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC ,       \
-    KC_LCBR, KC_A,     KC_S,     KC_D,   KC_F,  KC_G,    KC_ESC,                  KC_ASTR, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC,   \
+    KC_LGUI, KC_A,     KC_S,     KC_D,   KC_F,  KC_G,    KC_ESC,                  KC_ASTR, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC,   \
     KC_RCBR, KC_LSFT,  KC_Z,     KC_X,   KC_C,  KC_V,    KC_B,                    KC_PERC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,       \
     KC_ENT,  KC_BSPC,  KC_DEL,  KC_LALT,  KC_LSFT, KC_LCTL,KC_SPC,        KC_LALT, KC_LSFT, KC_SPC, KC_LCTL,     KC_LEFT, KC_DOWN,  KC_RGHT \
   ),
